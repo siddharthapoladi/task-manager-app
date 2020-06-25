6b87e7de0ad1606e04722cd204841c1066488576
Siddhartha Rao Poladi <siddhartha.poladi@gmail.com>
2020-06-25 23:03:41 +0530

    Changed Mongodb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://taskmanager:taskmanager123@cluster0-shard-00-00-c4jze.mongodb.net:27017,cluster0-shard-00-01-c4jze.mongodb.net:27017,cluster0-shard-00-02-c4jze.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL
     , {
         useNewUrlParser: true,
         useCreateIndex: true,
